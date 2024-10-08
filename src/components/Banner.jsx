888eada01cd42152ff6f7e377cc8421e7631272b
Shalini Maurya <shalini111maurya@gmail.com>
2024-10-09 03:29:50 +0530

    Update Banner.jsx

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ import Kiwi from '../assets/png/kiwi.png'
 import Leaf from '../assets/png/leaf.png'
 import Lemon from '../assets/png/lemon.png'
 import Tomato from '../assets/png/tomato.png'
-import PrimaryButton from './primaryButton'
+import PrimaryButton from './PrimaryButton'
 
 
 const Banner = () => {
